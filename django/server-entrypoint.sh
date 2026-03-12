5e0233c0244accfda038b7b651baee1453f525ea
Joao Urrunaga <u201913507@upc.edu.pe>
2026-03-12 12:39:34 -0500

    update entrypoint

--- django/server-entrypoint.sh
+++ django/server-entrypoint.sh
@@ -2,40 +2,56 @@
 
 set -e
 
-# Fix permissions on mounted volumes
 if [ "$(id -u)" = "0" ]; then
   mkdir -p htmlcov
   chown -R django:django htmlcov
   chown -R django:django /app/backend
 fi
 
-echo "Running Django migrations..."
-
-# Switch to django user and run commands
 if [ "$(id -u)" = "0" ]; then
   exec gosu django "$0" "$@"
 fi
 
-python manage.py wait_db
+check_db_running() {
+  python manage.py wait_db
+}
 
-if [ "$ENV_TYPE" = "development" ]; then
-  python manage.py makemigrations --noinput
-fi
+running_migrations() {
+  echo "Running Django migrations..."
 
-python manage.py migrate --fake-initial --noinput
+  if [ "$ENV_TYPE" = "development" ]; then
+    python manage.py makemigrations --noinput
+  fi
 
-if [ "$ENV_TYPE" = "production" ]; then
-  python manage.py collectstatic --noinput
-fi
+  python manage.py migrate --fake-initial --noinput
 
-# Run tests in development/test environments
-if [ "$ENV_TYPE" = "development" ] || [ "$ENV_TYPE" = "test" ]; then
-  echo "Running tests with coverage..."
-  rm -f /app/backend/.coverage
-  cd /app/backend
-  PYTHONPATH=/app/backend/src:$PYTHONPATH pytest test --cov=src/apps --cov-report=term-missing --cov-report=html:htmlcov --cov-config=.coveragerc || true
-  cd /app/backend/src
-fi
+  if [ "$ENV_TYPE" = "production" ]; then
+    python manage.py collectstatic --noinput
+  fi
+}
 
-echo "Starting server..."
-exec "$@"
+running_command_after_migrations() {
+  echo "Running command for creating groups and assing permissions"
+  python manage.py create_groups.py
+}
+
+running_tests() {
+  if [ "$ENV_TYPE" = "development" ] || [ "$ENV_TYPE" = "test" ]; then
+    echo "Running tests with coverage..."
+    rm -f /app/backend/.coverage
+    cd /app/backend
+    PYTHONPATH=/app/backend/src:$PYTHONPATH pytest test --cov=src/apps --cov-report=term-missing --cov-report=html:htmlcov --cov-config=.coveragerc || true
+    cd /app/backend/src
+  fi
+}
+
+main () {
+  check_db_running
+  running_migrations
+  running_tests
+
+  echo "Starting server..."
+  exec "$@"
+}
+
+main
