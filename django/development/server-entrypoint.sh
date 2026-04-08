ceb92196910ad1745404fb5bf52d5c3e7a4bc0f6
Joao Urrunaga <u201913507@upc.edu.pe>
2026-04-08 12:14:11 -0500

    add another layer on the folder structure

--- django/development/server-entrypoint.sh
+++ django/development/server-entrypoint.sh
@@ -19,39 +19,36 @@ check_db_running() {
 running_migrations() {
   echo "Running Django migrations..."
 
-  if [ "$ENV_TYPE" = "development" ]; then
-    python manage.py makemigrations --noinput
-  fi
-
+  python manage.py makemigrations --noinput
   python manage.py migrate --fake-initial --noinput
-
-  if [ "$ENV_TYPE" = "production" ]; then
-    python manage.py collectstatic --noinput
-  fi
 }
 
 running_command_after_migrations() {
   echo "Running command for creating groups and assing permissions"
-  python manage.py create_groups.py
+
+  python manage.py create_groups
+  # python manage.py create_currency
+  # python manage.py create_period
+  # python manage.py create_plan
+  # python manage.py create_price
 }
 
 running_tests() {
-  if [ "$ENV_TYPE" = "development" ] || [ "$ENV_TYPE" = "test" ]; then
-    echo "Running tests with coverage..."
-    rm -f /app/backend/.coverage
-    cd /app/backend
-    PYTHONPATH=/app/backend/src:$PYTHONPATH pytest test --cov=src/apps --cov-report=term-missing --cov-report=html:htmlcov --cov-config=.coveragerc || true
-    cd /app/backend/src
-  fi
+  echo "Running tests with coverage..."
+  rm -f /app/backend/.coverage
+  cd /app/backend
+  PYTHONPATH=/app/backend/src:$PYTHONPATH pytest test --cov=src/apps --cov-report=term-missing --cov-report=html:htmlcov --cov-config=.coveragerc || true
+  cd /app/backend/src
 }
 
 main () {
   check_db_running
   running_migrations
-  running_tests
+  running_command_after_migrations
+  # running_tests
 
   echo "Starting server..."
   exec "$@"
 }
 
-main
+main "$@"
